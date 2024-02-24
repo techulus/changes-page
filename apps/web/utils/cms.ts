19606d41347da51b2272b04bc38dcbd6445a8751
Arjun Komath <arjun@hey.com>
2024-02-24 11:15:01 +1100

    Fix Sanity config

--- apps/web/utils/cms.ts
+++ apps/web/utils/cms.ts
@@ -3,7 +3,7 @@ import imageUrlBuilder from "@sanity/image-url";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 const client = createClient({
-  projectId: process.env.SANITY_PROJECT_ID, // you can find this in sanity.json
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, // you can find this in sanity.json
   dataset: "production", // or the name you chose in step 1
   useCdn: true, // `false` if you want to ensure fresh data
 });
