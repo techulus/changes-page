ed5d466c631505a643290c8d8514bcd5ff8e3669
Arjun Komath <arjun@hey.com>
2024-06-27 18:57:17 +1000

    Ignore revalidate errors

--- apps/web/utils/revalidate.ts
+++ apps/web/utils/revalidate.ts
@@ -1,13 +1,17 @@
 import { httpPost } from "./http";
 
 export const revalidatePage = async (path: string) => {
-  const revalidateEndpoint =
-    process.env.REVALIDATE_ENDPOINT || "https://hey.changes.page";
+  try {
+    const revalidateEndpoint =
+      process.env.REVALIDATE_ENDPOINT || "https://hey.changes.page";
 
-  const result = await httpPost({
-    url: `${revalidateEndpoint}/api/revalidate?secret=${process.env.REVALIDATE_TOKEN}`,
-    data: { path },
-  });
+    const result = await httpPost({
+      url: `${revalidateEndpoint}/api/revalidate?secret=${process.env.REVALIDATE_TOKEN}`,
+      data: { path },
+    });
 
-  console.log("revalidatePage: Result:", path, result);
+    console.log("revalidatePage: Result:", path, result);
+  } catch (error) {
+    console.error("revalidatePage failed: Error:", error);
+  }
 };
