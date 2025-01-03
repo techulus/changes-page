5b6a960f3c54019d93a55a7ce65ecf9f95a9f4b3
Arjun Komath <arjun@hey.com>
2025-01-04 09:43:55 +1100

    Update Content Security Policy to allow fonts from fonts.gstatic.com

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,7 +4,7 @@ const ContentSecurityPolicy = `
   script-src 'self' 'unsafe-eval' 'unsafe-inline' *;
   style-src 'self' data: 'unsafe-inline' maxcdn.bootstrapcdn.com cdn.jsdelivr.net cdn.zapier.com fonts.googleapis.com;
   img-src 'self' * data: blob:;
-  font-src 'self' data: maxcdn.bootstrapcdn.com cdn.jsdelivr.net;
+  font-src 'self' data: maxcdn.bootstrapcdn.com cdn.jsdelivr.net fonts.gstatic.com;
   connect-src 'self' wss: *.supabase.co *.changes.page manageprompt.com zapier.com *.zapier.com www.google.com;
   worker-src 'self' blob:;
   report-to default
