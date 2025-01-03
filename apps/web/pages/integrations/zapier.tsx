a032807a9005ed1a50fabd64a3ffaa09a6157f7e
Arjun Komath <arjun@hey.com>
2025-01-03 23:10:41 +1100

    Update Zapier UI

--- apps/web/pages/integrations/zapier.tsx
+++ apps/web/pages/integrations/zapier.tsx
@@ -23,21 +23,18 @@ export default function Zapier() {
       </Head>
 
       <Page title="Zapier Integration" fullWidth>
-        <div className="relative pt-16 pb-32 overflow-hidden">
+        <div className="relative pb-32 overflow-hidden">
           <div className="relative">
             <div className="lg:mx-auto lg:max-w-7xl text-center">
-              <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-4 pb-8 lg:pb-16 lg:max-w-none lg:mx-0 lg:px-0">
-                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 dark:text-gray-50">
-                  Connect with Zapier and{" "}
-                  <span className="text-indigo-600 dark:text-indigo-500">
-                    automate your workflow
-                  </span>
-                  , here are some examples:
-                </h2>
-              </div>
+              <zapier-workflow
+                client-id="rnKv828fHE7sPhcZdGhwqWbIsJkOfhUEh2RAHQw4"
+                theme="auto"
+                intro-copy-display="show"
+                guess-zap-display="show"
+              />
             </div>
 
-            <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+            <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24 mt-24">
               <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
                 <div>
                   <div className="hidden lg:block">
@@ -124,17 +121,6 @@ export default function Zapier() {
                   />
                 </div>
               </div>
-            </div>
-          </div>
-
-          <div className="lg:mx-auto lg:max-w-7xl text-center mt-24">
-            <div className="px-4 max-w-xl mx-auto sm:px-6 py-8 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
-              <zapier-workflow
-                client-id="rnKv828fHE7sPhcZdGhwqWbIsJkOfhUEh2RAHQw4"
-                theme="auto"
-                intro-copy-display="show"
-                guess-zap-display="show"
-              />
             </div>
           </div>
         </div>
