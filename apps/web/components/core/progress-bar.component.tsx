afc76417ebabd6757f3163fa4bcf81a057e2bfc2
Arjun Komath <arjun@hey.com>
2025-02-11 07:40:47 +1100

    Add named function to progress bar component

--- apps/web/components/core/progress-bar.component.tsx
+++ apps/web/components/core/progress-bar.component.tsx
@@ -13,6 +13,6 @@ Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
-export default function () {
+export default function ProgressBar() {
   return null;
 }
