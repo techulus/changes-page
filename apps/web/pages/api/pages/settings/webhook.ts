a86b2cc15bcc10589d79e919b121ae10c81a4baf
Arjun Komath <arjun@hey.com>
2025-07-27 11:03:35 +1000

    Handle custom domain removal

--- apps/web/pages/api/pages/settings/webhook.ts
+++ apps/web/pages/api/pages/settings/webhook.ts
@@ -21,6 +21,25 @@ const databaseWebhook = async (req: NextApiRequest, res: NextApiResponse) => {
         page_id
       );
 
+      if (type === "DELETE") {
+        if (page_settings?.custom_domain) {
+          try {
+            const response = await fetch(
+              `https://api.vercel.com/v8/projects/${process.env.VERCEL_PAGES_PROJECT_ID}/domains/${page_settings.custom_domain}?teamId=${process.env.VERCEL_TEAM_ID}`,
+              {
+                headers: {
+                  Authorization: `Bearer ${process.env.VERCEL_AUTH_TOKEN}`,
+                },
+                method: "DELETE",
+              }
+            ).then((res) => res.json());
+            console.log("Response from Vercel API:", response);
+          } catch (error) {
+            console.error("Error deleting custom domain:", error);
+          }
+        }
+      }
+
       return res.status(200).json({ ok: true });
     } catch (err) {
       console.log("Trigger databaseWebhook [Page Settings]: Error:", err);
