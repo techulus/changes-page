a3b8604907258b56534255ac4506ce2d7c031d3a
Arjun Komath <arjunkomath@gmail.com>
2025-12-20 19:06:23 +1100

    Fix build

---
+++ packages/react-sdk/tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  dts: true,
+  clean: true,
+  external: ["react", "@changespage/core"],
+});
