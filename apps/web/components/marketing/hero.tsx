8315a254558ba50925ae84c120d7f45295f54c70
Arjun Komath <arjunkomath@gmail.com>
2025-12-22 19:55:59 +1100

    Update hero

--- apps/web/components/marketing/hero.tsx
+++ apps/web/components/marketing/hero.tsx
@@ -87,7 +87,7 @@ export default function Hero({
                 className="inline-flex space-x-6"
               >
                 <span className="rounded-full bg-indigo-500/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-400 ring-1 ring-inset ring-indigo-500/20">
-                  What&apos;s new
+                  NEW
                 </span>
                 <span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-300">
                   {latestPost ? (
